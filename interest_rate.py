3eb6071f2736bfaf647eedbd6562aa0129eb8a2f
gg2572 <ggao@simulmedia.com>
2018-05-11 12:27:42 -0400

    fix the plot ylim

--- interest_rate.py
+++ interest_rate.py
@@ -30,7 +30,6 @@ yield_curve = quandl.get('USTREASURY/YIELD', start_date='1984-05-09', end_date='
 
 yield_curve['10-2 yield spread'] = yield_curve['10 YR'] - yield_curve['2 YR']
 
-
 plt.plot_date(yield_curve.index, yield_curve['10-2 yield spread'], 'g-', label='10-2 yield spread', markersize=1)
 plt.plot_date(yield_curve.index, yield_curve['2 YR'], 'r-', label='2 YR', markersize=1, linewidth=1)
 plt.plot_date(yield_curve.index, yield_curve['10 YR'], 'b-', label='10 YR', markersize=1, linewidth=1)
@@ -38,9 +37,10 @@ plt.plot_date(yield_curve.index, yield_curve['10 YR'], 'b-', label='10 YR', mark
 vlines = {'title': ['Junk Bond Crash in 10/1989', 'Tequila Crisis 12/20/1994', 'Dot-com Bubble Crash 03/11/2000', 'Global Financial Crisis 08/09/2007', 'Today'],
           'date': ['1990-01-01', '1994-12-20', '2000-03-11', '2007-08-09', '2018-05-09']}
 
+ylim = [plt.gca().axes.get_ylim()[0], plt.gca().axes.get_ylim()[1]]
 for i in range(len(vlines['title'])):
     plt.text(datetime.strptime(vlines['date'][i], '%Y-%m-%d') + timedelta(days=5), plt.gca().axes.get_ylim()[1] / 2, vlines['title'][i], rotation=90, fontsize=6, color='k', verticalalignment='center')
-    plt.vlines(x=datetime.strptime(vlines['date'][i], '%Y-%m-%d'), ymin=plt.gca().axes.get_ylim()[0], ymax=plt.gca().axes.get_ylim()[1], color='k', linestyles='dashed', linewidth=0.5)
+    plt.vlines(x=datetime.strptime(vlines['date'][i], '%Y-%m-%d'), ymin=ylim[0], ymax=ylim[1], color='k', linestyles='dashed', linewidth=0.5)
 
 plt.xlabel('Date')
 plt.ylabel('Rate / %')
